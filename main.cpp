c31e3d41b028fb69feb2c547855e62e07ee6ac65
DEnispoip <mikuliakdenis1982@gmail.com>
2022-01-06 17:12:13 +0200

    second commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <cmath>
 
 void vivod(std::vector<int> pop);
-int deleteElement(std::vector<int> pop,int a);
+int deleteElement(std::vector<int>& pop,int a);
 
 int main()
 {
     std::vector<int> pop{4,4,7,90,1,74,985,5,54,12,412,12,425,51,23};
+    vivod(pop);
+    std::cout << deleteElement(pop, 6) << std:: endl;
     vivod(pop);
 
     return 0;
@@ -22,8 +25,11 @@ void vivod(std::vector<int> pop){
     std::cout<<std::endl;
 }
 
-int deleteElement(std::vector<int> pop,int a){
-    int chislo = ;
+int deleteElement(std::vector<int>& pop,int a){
+    int chislo = 0;
+    for(int i =0; i < 32; i++){
+        chislo += pow(2,i);
+    }
     for (std::vector<int>::iterator iterator_delete = pop.begin(); iterator_delete < pop.end(); iterator_delete++){
         if (a==*iterator_delete){
           chislo = *iterator_delete;
